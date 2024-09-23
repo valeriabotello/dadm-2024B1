a9522ccbc1e828a5f4cca25960e2c1a8223c334e
valeriabotello <valeria.botello.olvera2410@gmail.com>
2024-09-23 18:52:40 +0000

    ⭐ FEAT: ITEMS ul

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
 import { ref } from "vue";
 //modelo 
 const header = ref('App lista de Compras');
+const items = ref([
+{id:'0', label: '10 bolillos'},
+{id:'1',label: 'lata frijoles'},
+{id:'2',label: 'refrescos'},
+{id:'3',label: 'chetos'},
+{id:'4',label: 'paletas'}
+]);
 </script>
 
 <template>
@@ -9,6 +16,9 @@ const header = ref('App lista de Compras');
     <i class="material-icons shopping-cart-icon">local_mall</i>
     {{ header }}
     </h1>
+   <ul>
+   <li v-for="item in items" :key="item.id"> 🛒 {{item.label}} </li>
+    </ul>
 </template>
 
 <style scoped>
