b79e22e0b07b5fea0cb158be9309d12d6ed07b52
valeriabotello <valeria.botello.olvera2410@gmail.com>
2024-09-23 18:13:32 +0000

    FEAT: index link

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Proyecto didactico para el cuerso de
 Desarrollo de apps
 
 ## Autor 
-Enrique Ortega Celis 💀
+BOTELLO OLVERA STEPHANIE VALERIA
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
+import { ref } from "vue";
+//modelo 
+const header = ref('App lista de Compras');
 </script>
 
 <template>
-<h1>Shopping Cart</h1>
+<h1>
+    <i class="material-icons shopping-cart-icon">local_mall</i>
+    {{ header }}
+    </h1>
 </template>
 
 <style scoped>
+.shopping-cart-icon{
+    font-size: 2rem;
+}
 </style>
